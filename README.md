9a341b3649e907229884729596a0644d48ed754e
Ethan Knox <46270278+ethank5149@users.noreply.github.com>
2024-02-24 10:43:03 -0600

    fixed sympy to scipy handoff via lambdify

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # PyPendula
 ### Primary source code repository for PyPendula
+
+let:
+
+$$(x_1, y_1)=(      l\sin\theta_1,       l\cos\theta_1)$$
+$$(x_2, y_2)=(x_1 + l\sin\theta_2, y_1 + l\cos\theta_2)$$
+$$(x_3, y_3)=(x_2 + l\sin\theta_3, y_2 + l\cos\theta_3)$$
+
+s.t.:
+
+$$(\dot{x}_1, \dot{y}_1)=(            l\dot{\theta}_1\cos\theta_1,           -l\dot{\theta}_1\sin\theta_1)$$
+$$(\dot{x}_2, \dot{y}_2)=(\dot{x}_1 + l\dot{\theta}_2\cos\theta_2, \dot{y}_1 -l\dot{\theta}_2\sin\theta_2)$$
+$$(\dot{x}_3, \dot{y}_3)=(\dot{x}_2 + l\dot{\theta}_3\cos\theta_3, \dot{y}_2 -l\dot{\theta}_3\sin\theta_3)$$
+
+and subsequently:
+
+$$\begin{align*}
+  ||v_1||^2 &= l^2\left(\dot{\theta}^2_1\cos^2\theta_1 + \dot{\theta}^2_1\sin^2\theta_1\right) \\
+            &= l^2\dot{\theta}^2_1\left(\cos^2\theta_1 + \sin^2\theta_1\right) \\
+            &= l^2\dot{\theta}^2_1 \\
+\end{align*}$$
+
+$$\begin{align*}
+  ||v_2||^2 &= l^2\left[(\dot{\theta}_1\cos\theta_1 + \dot{\theta}_2\cos\theta_2)^2 + (\dot{\theta}_1\sin\theta_1 + \dot{\theta}_2\sin\theta_2)^2\right] \\
+            &= l^2\left[(\dot{\theta}_1\cos\theta_1 + \dot{\theta}_2\cos\theta_2)^2 + (\dot{\theta}_1\sin\theta_1 + \dot{\theta}_2\sin\theta_2)^2\right] \\
+\end{align*}$$
+
+$$\begin{align*}
+  ||v_3||^2 &= l^2\left[(\dot{\theta}_1\cos\theta_1 + \dot{\theta}_2\cos\theta_2 + \dot{\theta}_3\cos\theta_3)^2 + (\dot{\theta}_1\sin\theta_1 + \dot{\theta}_2\sin\theta_2 + \dot{\theta}_3\sin\theta_3)^2\right] \\
+            &= l^2\left[(\dot{\theta}_1\cos\theta_1 + \dot{\theta}_2\cos\theta_2 + \dot{\theta}_3\cos\theta_3)^2 + (\dot{\theta}_1\sin\theta_1 + \dot{\theta}_2\sin\theta_2 + \dot{\theta}_3\sin\theta_3)^2\right] \\
+\end{align*}$$
--- pypendula.py
+++ pypendula.py
@@ -1,75 +1,101 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.patches import Arc, Circle
+
 import sympy as sp
 from sympy.matrices import Matrix
 from sympy.physics.mechanics import dynamicsymbols, Lagrangian, LagrangesMethod, ReferenceFrame, Point, Particle
 import dill
 
 dill.settings['recurse'] = True
+rng = np.random.default_rng()
 
 class PyPendula:
-    def __init__(self, N=3, gravity=10, simplify=True, reduce=False):
+    def __init__(
+            self, 
+            N=3, 
+            params={
+                'm' : 1,
+                'g' : 10,
+                'l' : 1,
+            },
+            ics = None,
+            alpha=3,
+            ):
+        self.params = params
         self.N = N
-        self.gravity = gravity
+        self.alpha = alpha
 
-        q = dynamicsymbols(f'q:{self.N}') if self.N > 1 else [dynamicsymbols(f'q'),]
-        p = dynamicsymbols(f'q:{self.N}', level=1) if self.N > 1 else [dynamicsymbols(f'q', level=1),]
-        dp = dynamicsymbols(f'q:{self.N}', level=2) if self.N > 1 else [dynamicsymbols(f'q', level=2),]
-        l, m, g, t = sp.symbols('l, m, g, t')
+        if ics is None:
+            ics_q = rng.uniform(-np.pi / self.alpha, np.pi / self.alpha, size=self.N)
+            ics_p = np.zeros(self.N)
+            self.ics = np.hstack([ics_q, ics_p])
+        else:
+            self.ics = ics
 
-        # Compose World Frame
-        I = ReferenceFrame('I')
-        O = Point('O')
-        O.set_vel(I, 0)
+    def show_diagram(self):
+        assert self.N == 3
 
-        points = [O.locatenew('P_0', l * sp.sin(q[0]) * I.x - l * sp.cos(q[0]) * I.y),]
-        points[0].set_vel(I, points[0].pos_from(O).dt(I))
-        particles = [Particle('p_0', points[0], m),]
-        particles[0].potential_energy = m * g * (-l * sp.cos(q[0]))
 
-        # Append additional masses and apply properties
-        for _ in range(1, self.N):
-            points.append(O.locatenew(f'P_{_}', points[_ - 1].pos_from(O) + l * sp.sin(q[_]) * I.x - l * sp.cos(q[_]) * I.y))
-            points[_].set_vel(I, points[_].pos_from(O).dt(I))
-            particles.append(Particle(f'p_{_}', points[_], m))
-            particles[_].potential_energy = m * g * (-l * sp.cos(q[_]))
+        theta1, theta2, theta3, _, _, _ = self.ics
+        l = self.params['l']
 
-        # Calculate the lagrangian, and form the equations of motion
-        self.Lagrangian = Lagrangian(I, *particles)
-        self.LagrangesMethod = LagrangesMethod(self.Lagrangian, q, frame=I)
-        self.EulerLagrangeEqns = self.LagrangesMethod.form_lagranges_equations()
-        self.eom = self.LagrangesMethod.eom
-        self.ddq = self.solve_eom()
+        fig, ax = plt.subplots(figsize=(10, 10))
+        ax.set_xlim([-3.0 * l, 3.0 * l])
+        ax.set_ylim([-3.5 * l, 3.0 * l])
+
+        x0, y0 =                       0,                       0
+        x1, y1 =      l * np.sin(theta1),    - l * np.cos(theta1)
+        x2, y2 = x1 + l * np.sin(theta2), y1 - l * np.cos(theta2)
+        x3, y3 = x2 + l * np.sin(theta3), y2 - l * np.cos(theta3)
+
+        ax.vlines([0, x1, x2], [y1, y2, y3], [0, y1, y2], linestyles='dashed', colors='black', zorder=1)
+        ax.hlines([y1, y2, y3], [x1, x1, x2], [0, x2, x3], linestyles='dashed', colors='black', zorder=1)
+
+        # Rods
+        ax.plot([x0, x1], [y0, y1], lw=3, color='darkgray')  # , lw=2 * r_l, color='blue',  zorder=4)
+        ax.plot([x1, x2], [y1, y2], lw=3, color='darkgray')  # , lw=2 * r_l, color='red',   zorder=3)
+        ax.plot([x2, x3], [y2, y3], lw=3, color='darkgray')  # , lw=2 * r_l, color='green', zorder=2)
+
+        # Masses
+        ax.add_patch(Circle([x1, y1], radius=l / 6, color='blue', label=r'$(x_1,y_1)$',  zorder=4))
+        ax.text(x1, y1, r'$m_1$', zorder=10)
+
+        ax.add_patch(Circle([x2, y2], radius=l / 6, color='red', label=r'$(x_2,y_2)$', zorder=3))
+        ax.text(x2, y2, r'$m_2$', zorder=10)
+
+        ax.add_patch(Circle([x3, y3], radius=l / 6, color='green', label=r'$(x_3,y_3)$', zorder=2))
+        ax.text(x3, y3, r'$m_3$', zorder=10)
+
+        # Pins
+        ax.add_patch(Circle(             [x0, y0], radius=l / 12, color='black',  zorder=4))
         
-        if reduce:
-            self.reduce()
-            self.simplify()
-        elif simplify:
-            self.simplify()
+        # plt.legend()
 
-    def simplify(self):
-        self.eom = sp.simplify(self.eom)
-        self.ddq = sp.simplify(self.ddq)
+        return fig
 
-    def reduce(self):
-        self.ddq = self.ddq.subs([
-             (sp.symbols('l'), sp.Rational(1, self.N)), 
-             (sp.symbols('m'), sp.Rational(1, self.N)),
-             (sp.symbols('g'), self.gravity),
-             ])
-        self.eom = self.eom.subs([
-             (sp.symbols('l'), sp.Rational(1, self.N)), 
-             (sp.symbols('m'), sp.Rational(1, self.N)),
-             (sp.symbols('g'), self.gravity),
-             ])
+    def solve_symbolic(self):
+        q = dynamicsymbols(f'q:{self.N}')
+        dq = dynamicsymbols(f'q:{self.N}', level=1)
+        ddq = dynamicsymbols(f'q:{self.N}', level=2)
+        p = dynamicsymbols(f'p:{self.N}')
+        dp = dynamicsymbols(f'p:{self.N}', level=1)
+        l, m, g, t = sp.symbols('l m g t')
 
-    def solve_eom(self):
-        dp = dynamicsymbols(f'q:{self.N}', level=2) if self.N > 1 else [dynamicsymbols(f'q', level=2),]
-        return Matrix([
-            sp.solve(self.eom[_], dp[_])[0] for _ in range(self.N)
-        ])
-        
-    # def lambdify(self):
-    #     q = dynamicsymbols(f'q:{self.N}') if self.N > 1 else [dynamicsymbols(f'q'),]
-    #     p = dynamicsymbols(f'q:{self.N}', level=1) if self.N > 1 else [dynamicsymbols(f'q', level=1),]
-    #     t = sp.symbols('t')
-    #     self.ode_system = sp.utilities.lambdify([t, [*q, *p]], [*p, *self.ddq])
-    #     dill.dump(self.ode_system, open(f"./cache/pypendula_cached_soln_n{self.N}", "wb"))
+        ###################
+        x, y = [l * sp.sin(q[0])], [- l * sp.cos(q[0])]
+        for i in range(1, N):
+            x.append(x[i - 1] + l * sp.sin(q[i]))
+            y.append(y[i - 1] - l * sp.cos(q[i]))
+    
+        v_sqr = Matrix([_x.diff(t) ** 2 + _y.diff(t) ** 2 for _x,_y in zip(x, y)])
+        x, y = Matrix(x), Matrix(y)
+        ####################
+
+        potential = -m * g * sum(y)
+        kinetic = sp.Rational(1, 2) * m * sum(v_sqr)
+        lagrangian = kinetic - potential
+        self.lagranges_method = LagrangesMethod(lagrangian, q)
+        self.euler_lagrange_eqns = self.lagranges_method.form_lagranges_equations()
+        self.eom = sp.simplify(self.lagranges_method.eom.subs([(dq[_], p[_]) for _ in range(self.N)]))
+        self.symbolic_dp = Matrix(list(sp.solve(self.eom, *dp).values()))
